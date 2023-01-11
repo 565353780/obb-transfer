f2be10f2f9fc9be917c4ccfa9d82fed75cee938a
chLi <565353780@qq.com>
2023-01-11 13:26:24 +0800

    update setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,8 @@
+cd ..
+git clone git@github.com:565353780/noc-transform.git
+
+cd noc-transform
+./dev_setup.sh
+
+pip install open3d scipy
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd ..
+git clone https://github.com/565353780/noc-transform.git
+
+cd noc-transform
+./setup.sh
+
+pip install open3d scipy
+
